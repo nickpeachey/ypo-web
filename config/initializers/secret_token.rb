18ca6455d4cb398c1c92e307b3c76d8091d5e67f
nickpeachey <nick.peachey@gmail.com>
2014-02-05 10:41:19 +0000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YpoWeb::Application.config.secret_token = '817cce93d299cdc738a02df8e67492bee913b8b46335dfefa57f00b58d810c390449d50e5596e4e4692bf29dcefb4e8e04a5a65509eed396116f137aa50e2d56'
